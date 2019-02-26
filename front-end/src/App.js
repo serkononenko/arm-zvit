55eb1d273c0f80ea500efb737c1bb3940e23585b
Sergiy Kononenko <serkononenko@gmail.com>
2019-02-26 16:15:18 +0200

    Re-writing App component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,21 +1,32 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {  Route, Switch, Redirect } from 'react-router-dom';
 
 const Header = React.lazy(() => import('./Components/Header/Header'));
 const RegistrationForm = React.lazy(() => import('./Components/Forms/RegistrationForm/RegistrationForm'));
 const LogonForm = React.lazy(() => import('./Components/Forms/LogonForm/LogonForm'));
+
+import ContentLayout from './Layouts/ContentLayout/ContentLayout';
+
 import './App.css';
 
-export default function App() {
-    return (
-        <React.Fragment>
-            <Header />
-            <Router>
-                <Switch>
-                    <Route exact path="/" component={ LogonForm } />
-                    <Route exact path="/registration" component={ RegistrationForm } />
-                </Switch>
-            </Router>
-        </React.Fragment>
-    )
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loggedIn: false
+        };
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <Header />
+                    <Switch>
+                        <Route exact path="/" render={ () => (this.state.loggedIn ? (<ContentLayout/>) : (<Redirect to="/login"/>))}/>
+                        <Route exact path="/login" render={ (props) =>  <LogonForm {...props} /> } />
+                        <Route exact path="/registration" render={ (props) => <RegistrationForm {...props} /> } />
+                    </Switch>
+            </React.Fragment>
+        )
+    }
 };
