668020985126fd76930bc117ebb93b3c53a2a573
Sergiy Kononenko <serkononenko@gmail.com>
2019-02-26 16:16:03 +0200

    Re-writing FormLayout component

--- front-end/src/Layouts/FormLayout/FormLayout.js
+++ front-end/src/Layouts/FormLayout/FormLayout.js
@@ -2,16 +2,12 @@ import React from 'react';
 
 import './FormLayout.css';
 
-export default class FormLayout extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div className='FormLayout'>
-                {this.props.children}
-            </div>
-        )
-    }
+const FormLayout = (props) => {
+    return (
+        <div className='FormLayout'>
+            {props.children}
+        </div>
+    )
 }
+
+export default FormLayout;
