2fa88b5410730492b34f5a655a9de082139a3a36
Sergiy Kononenko <serkononenko@gmail.com>
2019-02-26 16:14:26 +0200

    Added ContenLayout

---
+++ front-end/src/Layouts/ContentLayout/ContentLayout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ContentLayout.css';
+
+const ContentLayout = () => {
+    return (
+        <div className='ContentLayout'>
+            Hello World
+        </div>
+    )
+}
+
+export default ContentLayout;
