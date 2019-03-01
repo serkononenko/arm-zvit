9ef090beba71bb037e5655f37b22df592581f62f
Sergiy Kononenko <serkononenko@gmail.com>
2019-03-01 14:44:57 +0200

    Updated find function (async/await)

--- back-end/db/userBaseSchema.js
+++ back-end/db/userBaseSchema.js
@@ -13,56 +13,46 @@ const UserBase = mongoose.model('userBase', userBaseChema);
 
 function regUser(req, res) {
     const body = req.body;
-    findUser(body.login)
-        .then((result) => {
+    const result = findUser(body.login)
             if (!!result) {
                 res.status(403).send('Forbidden');
             } else {
                 addUser(body);
                 res.status(200).send('OK');
             };
-//          console.log(result);
-        });
+
 }
 
 function logIn(req, res) {
     const body = req.body;
-    findUser(body.login)
-        .then((result) => {
-            if (!result) {
-                res.status(403).send('Forbidden');
+    const result = findUser(body.login);
+        if (!result) {
+            res.status(403).send('Forbidden');
+        } else {
+            if (body.password === result.password) {
+                res.status(200).send('OK');
             } else {
-                if (body.password === result.password) {
-                    res.status(200).send('OK');
-                } else {
-                    res.status(403).send('Forbidden');
-                }
+                res.status(403).send('Forbidden');
             }
-
-        })
+        }
 }
 
-function addUser(body) {
+async function addUser(body) {
     const userData = new UserBase({
         login: body.login,
         password: body.password,
         department: body.department
     });
-    userData.save();    
+    await userData.save();    
 }
 
-function findUser(login) {
-    var query = UserBase.findOne({ login });
-    return new Promise((resolve, reject) => {
-        query.exec((err, result) => {
-            if (err) reject(err);
-            resolve(result);
-        });
-    });
+async function findUser(login) {
+    const result = await UserBase.findOne({ login });
+    return result;
 }
 
-function deleteUser(userName) {
-    UserBase.findOneAndDelete({login: userName}, function(err, data) {
+async function deleteUser(userName) {
+    await UserBase.findOneAndDelete({login: userName}, function(err, data) {
         if (err) console.error(err)
         else console.log('deleted login: ' + data.login);
     });
