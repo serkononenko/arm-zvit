9f56b4273f5c5fa3cbd6c3ded8921df994dcce3a
Sergiy Kononenko <serkononenko@gmail.com>
2019-03-01 11:57:29 +0200

    Updated DepartmentList component

--- front-end/src/Components/DepartmentList/DepartmentList.js
+++ front-end/src/Components/DepartmentList/DepartmentList.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import './DepartmentList.css';
 
+const departmentUl = [
+    {department: 'Риба', departmentReport: [
+        'Карась', 'Товстолоб', 'Щука'
+    ], _id: 1},
+    {department: "М'ясо", departmentReport: [
+        'Курка', 'Свинина', 'Телятина'
+    ], _id: 2},
+    {department: 'Фрукт', departmentReport: [
+        'Вишня', 'Яблуко', 'Груша'
+    ], _id: 3},
+    {department: 'Овоч', departmentReport: [
+        'Морква', 'Помідор', 'Огірок'
+    ], _id: 4}
+];
+
 export default class DepartmentList extends React.Component {
     constructor(props) {
         super(props);
@@ -9,7 +24,7 @@ export default class DepartmentList extends React.Component {
         };
     }
 
-    getDeparnmentList(url) {
+/*    getDeparnmentList(url) {
         fetch(url, {method: 'GET'})
             .then((res) => {
                 res.json().then((data) => {
@@ -23,9 +38,20 @@ export default class DepartmentList extends React.Component {
     componentDidMount() {
         this.getDeparnmentList('/getDepartmentList');
     }
-
+*/
     render() {
-        const departmentList = this.state.departmentList.map((item) => <li>{item.department}</li>)
+//        const departmentList = this.state.departmentList.map((item) => <li>{item.department}</li>)
+    const departmentList = departmentUl.map((item, index) => {
+        const subList = item.departmentReport.map((subItem, index2) => <a href='#' key={index2}>{subItem}</a>)
+        return (
+            <li key={index} id={item._id} className='DepartmentList__item'>
+                <a href={'#'+item._id} className='DepartmentList__link'>{item.department}</a>
+                <div className='DepartmentList__sublist'>
+                    {subList}
+                </div>
+            </li>
+        )
+    })
         return (
             <ul className='DepartmentList'>
                 {departmentList}
