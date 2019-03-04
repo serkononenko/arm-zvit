65aa76ff7681465ca23706ee1ab59915cfd7120e
Sergiy Kononenko <serkononenko@gmail.com>
2019-03-04 15:11:03 +0200

    component updated

--- view/src/index.js
+++ view/src/index.js
@@ -1,6 +1,7 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { Router } from 'react-router-dom'
+import createBrowserHistory from "history/createBrowserHistory";
 
 import MainLayout from './Layouts/MainLayout/MainLayout';
 
@@ -9,10 +10,11 @@ import './index.css';
 const App = React.lazy(() => import('./App'));
 const elem =  <Suspense fallback={<div>Loading...</div>}>
                 <App />
-              </Suspense>;             
+              </Suspense>;      
+const history = createBrowserHistory()       
  
 ReactDOM.render(
-    <Router>
+    <Router history={history}>
       <MainLayout children = { elem } />
     </Router>,
     document.getElementById('root')
