000adc1435df8bf54c2f66d30ba4907768c1b21b
Sergiy Kononenko <serkononenko@gmail.com>
2019-03-04 15:10:16 +0200

    refactoring code

---
+++ controllers/userController.js
@@ -0,0 +1,54 @@
+const User = require('../models/userbase');
+
+const user_list = (req, res) => {
+    User.find({}, (err, result) => {
+        if (err) console.error(err)
+        else res.send(result);
+    });
+};
+
+const user_create = (req, res) => {
+    const { login, password, department } = req.body;
+    const userData = new User({
+        login,
+        password,
+        department
+    });
+    userData.save((err) => {
+        if (err) console.error(err)
+        else res.status(200).send('OK');
+    });
+};
+
+const user_delete = (req, res) => {
+    const { login } = req.body;
+    User.findOneAndDelete({login}, (err, result) => {
+        if (err) console.error(err)
+        else res.status(200).send('OK');
+    })
+}
+
+const register_user = (req, res) => {
+    const result = findUser(req.body.login);
+    if (!!result) {
+        res.status(403).send('Forbidden');
+    } else user_create(req, res);
+}
+
+const login_user = (req, res) => {
+    const { login, password } = req.body;
+    const result = findUser(login);
+    if (!result) {
+        res.status(401).send('Unauthorized');
+    } else {
+        if (password === result.password) {
+            res.status(200).send('OK');
+        } else {
+            res.status(403).send('Forbidden');
+        }
+    }
+}
+
+async function findUser(login) {
+    return await User.findOne({ login });
+}
--- models/userbase.js
+++ models/userbase.js
@@ -2,61 +2,18 @@ const mongoose = require('mongoose');
 
 //create Shema
 const userBaseChema = new mongoose.Schema({
-    login: String,
+    login: {
+        type: String,
+        unique: true
+    },
     password: String,
-    department: String,
+    department: {
+        type: Schema.ObjectId,
+        ref: 'departmentBase',
+        required: true
+    },
     isAdmin: Boolean
 });
 
 //create & export Model
 module.exports = mongoose.model('userBase', userBaseChema);
-
-
-
-function regUser(req, res) {
-    const body = req.body;
-    const result = findUser(body.login)
-            if (!!result) {
-                res.status(403).send('Forbidden');
-            } else {
-                addUser(body);
-                res.status(200).send('OK');
-            };
-
-}
-
-async function logIn(req, res) {
-    const body = req.body;
-    const result = await findUser(body.login);
-    console.log(result);
-        if (!result) {
-            res.status(401).send('Unauthorized');
-        } else {
-            if (body.password === result.password) {
-                res.status(200).send('OK');
-            } else {
-                res.status(403).send('Forbidden');
-            }
-        }
-}
-
-async function addUser(body) {
-    const userData = new UserBase({
-        login: body.login,
-        password: body.password,
-        department: body.department
-    });
-    await userData.save();    
-}
-
-async function findUser(login) {
-    const result = await UserBase.findOne({ login });
-    return result;
-}
-
-async function deleteUser(userName) {
-    await UserBase.findOneAndDelete({login: userName}, function(err, data) {
-        if (err) console.error(err)
-        else console.log('deleted login: ' + data.login);
-    });
-}
