fa300009957330c5da8792634051ca44a5eb2866
Sergiy Kononenko <serkononenko@gmail.com>
2019-03-13 10:04:52 +0200

    updated component

--- view/src/Components/Header/Header.js
+++ view/src/Components/Header/Header.js
@@ -1,20 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import LoggonedUser from '../LoggonedUser/LoggonedUser';
 import './Header.css';
 import logo from './logo.png';
-import gerb from './gerb.png';
 
-export default function Header() {
+function Header(props) {
+    const elem = props.user ? (
+        <div className = 'header__loggoned-user'>
+            <LoggonedUser user={props.user}/>
+        </div>
+        ) : (
+            <div></div>
+        )
     return (
         <div className='header'>
-            <div className = 'header__logo'>
-                <img src = {logo} className = "logo__top" />
-                <img src = {gerb} className = "logo__layout" />
+            <div className='header__logo-container'>
+                <img src = {logo} className = 'header__logo' />
             </div>
             <div className = 'header__title'>
                 <h1>АРМ - ЗВІТ</h1>
                 <span>Головне управління Пенсійного фонду України в Черкаській області</span>
             </div>
+            {elem}
         </div>
     )
 }
+
+const mapStateToProps = (state) => {
+    const user = state.logon.loggedIn;
+    return {
+        user
+    }
+}
+
+export default connect(mapStateToProps)(Header);
 
