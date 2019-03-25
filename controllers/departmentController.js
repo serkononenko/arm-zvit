11721e3ce8828f29bb7715fc385ac8955469d8d0
Sergiy Kononenko <serkononenko@gmail.com>
2019-03-25 15:15:55 +0200

    remove report field from response

--- controllers/departmentController.js
+++ controllers/departmentController.js
@@ -1,7 +1,7 @@
 const Department = require('../models/departmentbase');
 
 const department_list = (req, res) => {
-    const query = Department.find();
+    const query = Department.find(null, '_id name');
     query.exec((err, result) => {
         if (err) console.error(err)
         else res.send(result);
