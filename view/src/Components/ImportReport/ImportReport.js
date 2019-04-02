b9ba68257a81f309f3fa882449bd5411c020df94
Sergiy Kononenko <serkononenko@gmail.com>
2019-04-02 15:24:43 +0300

    added Import Report component

---
+++ view/src/Components/ImportReport/ImportReport.js
@@ -0,0 +1,16 @@
+import React, { useRef } from 'react';
+
+export default function ImportReport(props) {
+    const inputRep = useRef(null);
+    const onChange = () => {
+        const file = inputRep.current.files[0];
+    }
+    return (
+        <form onChange={onChange}>
+            <div className='form-group'>
+                <label htmlFor='input-report'>Імпортувати звіти</label>
+                <input type='file' className='form-control-file' id='input-report' accept='.csv' ref={inputRep}/>
+            </div>
+        </form>
+    )
+}
--- view/src/Components/MainPage/MainPage.js
+++ view/src/Components/MainPage/MainPage.js
@@ -4,11 +4,13 @@ import ContentLayout from '../../Layouts/ContentLayout/ContentLayout';
 import DepartmentInput from '../Forms/DepartmentForm/DepartmentInput';
 import DepartmentList from '../DepartmentList/DepartmentList';
 import UserList from '../UserList/UserList';
+import ImportReport from '../ImportReport/ImportReport';
 
 export default function MainPage(props) {
     return (
         <ContentLayout>
             <DepartmentInput />
+            <ImportReport />
             <UserList />
             <DepartmentList />
         </ContentLayout>
