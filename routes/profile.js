9fcd30fdf99d53174dce0dd27331b14e68ccce7e
Sergiy Kononenko <serkononenko@gmail.com>
2019-04-10 10:59:33 +0300

    updatetd profile route

--- routes/profile.js
+++ routes/profile.js
@@ -1,18 +1,15 @@
 const express = require('express');
-const dir = require('../dirname').rootDir;
 const router = express.Router();
+const bodyParser = require('body-parser');
+const dir = require('../dirname').rootDir;
 
-const user_controller = require('../controllers/userController');
+const profile_controller = require('../controllers/profileController');
 
 router.use(express.static(dir + '/view/dist/'));
-/*
-router.get('/', (req, res) => {
-    res.sendFile(path.dirname( __dirname) + '/view/dist/index.html');
-})
-*/
+router.use(bodyParser.text());
 
-router.get('/*', user_controller.user_profile);
+router.get('/:id', profile_controller.user_profile);
 
-router.post('/*', user_controller.user_profile_update);
+router.post('/:id', profile_controller.user_profile_update);
 
 module.exports = router;
