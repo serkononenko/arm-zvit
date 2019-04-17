97ab34c68a878f6e210e3a4c7ba8d8f03100691a
Sergiy Kononenko <serkononenko@gmail.com>
2019-04-17 12:34:08 +0300

    added redux dev tools

--- view/src/store.js
+++ view/src/store.js
@@ -1,7 +1,12 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import ReduxThunk from 'redux-thunk';
 import rootReduser from './reducers/rootReducer';
 
-let store = createStore(rootReduser, applyMiddleware(ReduxThunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+    rootReduser,
+    composeEnhancers(applyMiddleware(ReduxThunk))
+);
 
 export default store;
