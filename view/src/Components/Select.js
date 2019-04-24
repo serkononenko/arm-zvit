a3e9f26ced8680c4b856445be7fcb8a6644a0c59
Sergiy Kononenko <serkononenko@gmail.com>
2019-04-24 09:51:51 +0300

    added new component

---
+++ view/src/Components/Select.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Select = ({ data, value, handleChange, id }) => {
+    return (
+        <select 
+            className='form-control custom-select' 
+            id={id}
+            name={id} 
+            value={value}
+            onChange={handleChange}
+        >
+            { 
+                data.map((item) => 
+                    <option key={item._id} value={item._id}>
+                        {item.name}
+                    </option>
+                )
+            }
+        </select>
+    );
+};
+
+Select.propTypes = {
+    data: PropTypes.array,
+    value: PropTypes.string,
+    handleChange: PropTypes.func,
+    id: PropTypes.string
+};
+
+export default Select;
