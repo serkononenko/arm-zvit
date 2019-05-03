74ee2524a22e7c4ffd5e8fa0ef29173ec1bcabcc
Sergiy Kononenko <serkononenko@gmail.com>
2019-05-03 15:09:49 +0300

    added dotenv, cors, and route paths

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
+const PORT = process.env.PORT || 3000;
 const express = require('express');
 const app = express();
 const path = require('path');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors');
 const accessControl = require('./middleware/accessControl');
 const createDb = require('./utils/dbSetting');
 const logStream = require('./utils/Log');
@@ -13,12 +15,18 @@ const apiRouter = require('./routes/api');
 
 createDb();
 
-app.use(helmet());
-app.use(morgan('short', {stream: logStream('main.log')}));
+app.use(helmet()); //  for secure Express apps by setting various HTTP headers
+app.use(bodyParser.text());
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(cors()); // enabling CORS for all requests
+app.use(morgan('common', {stream: logStream('main.log')}));
 
-app.use(express.static(path.join(__dirname, '/view/dist/')));
+
+app.use(express.static(path.join(__dirname, 'view', 'dist')));
+app.use('/administrator', express.static(path.join(__dirname, 'view', 'dist')));
+app.use('/profile', express.static(path.join(__dirname, 'view', 'dist')));
+app.use('/department', express.static(path.join(__dirname, 'view', 'dist')));
 
 app.use(accessControl);
 
@@ -29,6 +37,6 @@ app.get('/*', function (req, res) {
 });
 
 
-app.listen(process.env.PORT, ()=> {
-    console.log('ARM-ZVIT Server listening on '+ process.env.PORT +' port');
+app.listen(PORT, ()=> {
+    console.log('ARM-ZVIT Server listening on '+ PORT +' port');
 });
